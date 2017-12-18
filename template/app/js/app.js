78386296fc59f62a7b0f0348371cfe74c26e1e9e
Sebastian Zilles <dev@zilles.io>
2017-12-18 17:40:52 +0100

    Init instance 2.0

---
+++ template/app/js/app.js
@@ -0,0 +1,30 @@
+// Styles:
+import '../scss/style.scss';
+
+// open.DASH:
+import openDASH from 'opendash';
+
+// User Adapter:
+/*! opendash-template-custom-user-adapter-app-i !*/
+
+// Data Adapter:
+/*! opendash-template-custom-data-adapter-app-i !*/
+
+// Widgets:
+/*! opendash-template-custom-data-widgets-app-i !*/
+
+const instance = new openDASH();
+
+// instance.env('OD-EVENTS-LOG', true);
+
+/*! opendash-template-custom-user-adapter-app !*/
+
+/*! opendash-template-custom-data-adapter-app !*/
+
+/*! opendash-template-custom-widgets-app !*/
+
+// App initialisieren:
+instance.start();
+
+// App initialisieren:
+const app = angular.module('app', [instance.name]);
